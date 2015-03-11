494dbc738426c3b6aa9cc27f68c981dc75c5e3e1
Sam Broner <sbro727@gmail.com>
2015-03-10 22:10:19 -0400

    Final updates

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,35 @@
-// Your JavaScript goes here...
+function parse() {
+
+        xhr = new XMLHttpRequest();
+        xhr.overrideMimeType("application/json");
+        xhr.open("get", "http://messagehub.herokuapp.com/messages.json", true);
+
+        xhr.onreadystatechange = loadMessages;
+
+
+        
+        function loadMessages(){
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                        console.log("In my callback function " + xhr);
+                        data = JSON.parse(xhr.responseText);
+
+                        var div = document.getElementById('messages');
+                        // div.innerHTML = '<div class="check">';
+                        // console.log(div.innerHTML);
+
+                        for(id in data) {
+                                div.innerHTML= div.innerHTML + '<p class="message"> <a class="content">' +
+                                                data[id].content.trim() + '</a> <a class="usrname">' +
+                                                 data[id].username.trim() + '</a> </p>';
+                        }
+
+                        console.log(div.innerHTML);
+                        // div.innerHTML = div.innerHTML.append('</div>') ;
+
+                }
+        }
+
+        xhr.send();
+
+
+};
