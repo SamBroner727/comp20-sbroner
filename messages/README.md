13df393a01575bb3c59fc68315edf510a8e3a1f0
Sam Broner <sbro727@gmail.com>
2015-03-10 22:51:13 -0400

    change json origin

---
+++ messages/README.md
@@ -0,0 +1,18 @@
+Sam Broner
+3/10/2015
+Lab 6
+
+I believe all aspects of the work have been implemented correctly, I 
+experimented a little with css on top of the required work. Specifically, 
+I was wondering how you would add classes to each element (content and username)
+so you could put them on different lines.
+
+I spent about 4 hours on the assignment.
+
+
+Browsers have implemented a same-origin policy that make sure javascript
+only accesses data from the same origin. Same origin means that the URLs
+have the same protocol, host, and port. Therefore, my implementation of 
+accessing data would not and does not work unless I disable web security.
+Disabling websecurity lets the browser run javascript such that it can 
+request data from other origins.
--- messages/lab.js
+++ messages/lab.js
@@ -2,7 +2,7 @@ function parse() {
 
         xhr = new XMLHttpRequest();
         xhr.overrideMimeType("application/json");
-        xhr.open("get", "data.json", true);
+        xhr.open("get", "http://messagehub.herokuapp.com/messages.json", true);
 
         xhr.onreadystatechange = loadMessages;
 
@@ -10,18 +10,16 @@ function parse() {
         
         function loadMessages(){
                 if (xhr.readyState == 4 && xhr.status == 200) {
-                        console.log("In my callback function " + xhr);
                         data = JSON.parse(xhr.responseText);
 
                         var div = document.getElementById('messages');
-                        div.innerHTML = '<div>';
-                        
+
                         for(id in data) {
-                                div.innerHTML= div.innerHTML + '<p class="message"> <span class="content">' +
-                                                data[id].content + '</span> <span class="usrname">' 
-                                                + data[id].username + '</span> </p>';
+                                div.innerHTML= div.innerHTML + '<p class="message"> <a class="content">' +
+                                                data[id].content.trim() + '</a> <a class="usrname">' +
+                                                 data[id].username.trim() + '</a> </p>';
                         }
-                        div.innerHTML = div.innerHTML + '</div>';
+
 
                 }
         }
