9361fd59b43d5f70dd667e9facaabc41373377d9
Sam Broner <sbro727@gmail.com>
2015-03-13 15:34:44 -0400

    typo, changing how popup is called

--- messages/lab.js
+++ messages/lab.js
@@ -14,7 +14,7 @@ function parse() {
         if (xhr.readyState == 4 && xhr.status == 200) {
             data = JSON.parse(xhr.responseText);
 
-            var div = document.getElementById('messages');
+            var div = document.getElementById('messages');   
 
             for (id in data) {
                 div.innerHTML = div.innerHTML + '<p class="message"> <a class="content">' +
@@ -30,3 +30,25 @@ function parse() {
 
 
 };
+
+function makeMarkers() {
+    
+    console.log(http.responseText);
+
+    var data = JSON.parse(http.responseText);
+
+    var marker;
+    var latlng
+
+    for(var user in data) {
+
+        latlng = new google.maps.LatLng(user.lat, user.lng);
+        marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+            title: user.login
+        });
+        marker.setMap(map);
+    }   
+
+}
